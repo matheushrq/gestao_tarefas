a3c24361016eaa3a91dfa78645a80fafd235e629
Matheus Henrique <henriquem320@gmail.com>
2026-01-08 22:32:01 -0300

    master: Arquivo de consulta para práticas diárias.

---
+++ consultas.sql
@@ -0,0 +1,26 @@
+IF OBJECT_ID('tempdb..#TempUsuarios') IS NOT NULL
+begin
+    DROP TABLE #TempUsuarios;
+end
+
+CREATE TABLE #TempUsuarios
+(
+    Nome NVARCHAR(100),
+    Email NVARCHAR(100)
+);
+
+bulk insert #TempUsuarios
+from 'C:\Git\projeto\task_management\lista.csv'
+with
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2
+);
+
+INSERT INTO Usuarios (Nome, Email)
+SELECT Nome, Email FROM #TempUsuarios;
+
+DROP TABLE #TempUsuarios;
+
+SELECT * FROM Usuarios
