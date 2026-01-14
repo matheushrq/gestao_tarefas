0587ef3d5bf05b2457dc7d9899eb5d5c2459a424
Matheus Henrique <henriquem320@gmail.com>
2026-01-14 15:26:37 -0300

    master: Implementação de tratamento de erros e performance da procedure.

--- stored_procedures/sp_exclui_tarefa.sql
+++ stored_procedures/sp_exclui_tarefa.sql
@@ -1,8 +1,24 @@
-CREATE PROCEDURE DeleteTask
-    @TaskId INT
+CREATE OR ALTER PROCEDURE dbo.sp_exclui_tarefa
+    @TarefaId INT
 AS
 BEGIN
-    DELETE FROM Tarefas
-    WHERE TarefaID = @TaskId
+    begin try
+        DELETE FROM Tarefas
+        WHERE TarefaID = @TarefaId
+    end try
+    begin catch
+        -- tratamento de erros
+
+        DECLARE @ErrorMessage   NVARCHAR(4000);
+        DECLARE @ErrorSeverity  INT;
+        DECLARE @ErrorState     INT;
+
+        SELECT
+            @ErrorMessage   = ERROR_MESSAGE(),
+            @ErrorSeverity  = ERROR_SEVERITY(),
+            @ErrorState     = ERROR_STATE();
+
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+    end catch
 END
 GO
