ef53de8ad3eeab6dd7ff1112308b05d8daffe2cf
Matheus Henrique <henriquem320@gmail.com>
2026-01-20 21:36:29 -0300

    master: Padronização de nomes e valores.

--- triggers/trg_prevent_delete_completed.sql
+++ triggers/trg_prevent_delete_completed.sql
@@ -1,13 +1,13 @@
 -- Trigger para impedir exclusão de tarefas concluídas
-CREATE TRIGGER trg_prevent_delete_completed
-ON tarefas
+CREATE  TRIGGER trg_prevent_delete_completed
+ON      Tarefas
 INSTEAD OF DELETE
 AS
 BEGIN
-    IF EXISTS (SELECT 1 FROM deleted WHERE status = 'concluída')
+    IF EXISTS (SELECT 1 FROM deleted WHERE StatusID = 3)
     BEGIN
         RAISERROR ('Não é permitido excluir tarefas concluídas.', 16, 1);
         RETURN;
     END
-    DELETE FROM tarefas WHERE id_tarefa IN (SELECT id_tarefa FROM deleted WHERE status <> 'concluída');
+    DELETE FROM tarefas WHERE TarefaID IN (SELECT TarefaID FROM deleted WHERE StatusID <> 3);
 END;
