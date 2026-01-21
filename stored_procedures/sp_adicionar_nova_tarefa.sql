d02e0dd9c54148d286a92e4b81d643108a029431
Matheus Henrique <henriquem320@gmail.com>
2026-01-20 21:31:16 -0300

    master: Adicionando validação para garantir que a data do prazo não seja menor que a data de criação da tarefa.

--- stored_procedures/sp_adicionar_nova_tarefa.sql
+++ stored_procedures/sp_adicionar_nova_tarefa.sql
@@ -5,6 +5,7 @@ GO
 CREATE PROCEDURE dbo.sp_adicionar_nova_tarefa
     @titulo         NVARCHAR(100),
     @Descricao      NVARCHAR(255),
+    @DataCriacao    DATETIME,
     @DataPrazo      DATETIME,
     @StatusID       INT,
     @UsuarioID      INT
@@ -12,9 +13,17 @@ AS
 BEGIN
     begin try
         INSERT INTO Tarefas (Titulo, Descricao, DataCriacao, DataPrazo, StatusID, UsuarioID)
-        VALUES (@titulo, @Descricao, GETDATE(), @DataPrazo, @StatusID, @UsuarioID)
+        VALUES (@titulo, @Descricao, @DataCriacao, @DataPrazo, @StatusID, @UsuarioID)
+
+        if (@DataPrazo < @DataCriacao)
+        BEGIN
+            RAISERROR('A data de prazo não pode ser anterior a data de criação.', 16, 1);
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
     end try
     begin catch
+
         -- tratamento de erros
         DECLARE @ErrorMessage NVARCHAR(4000);
         DECLARE @ErrorSeverity INT;
@@ -24,6 +33,7 @@ BEGIN
             @ErrorMessage = ERROR_MESSAGE(),
             @ErrorSeverity = ERROR_SEVERITY(),
             @ErrorState = ERROR_STATE();
+            
     end catch
 END
 GO
